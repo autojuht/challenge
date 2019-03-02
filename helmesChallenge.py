a3e3187a4fc1e151c75f18f95bda9fbbff871b6b
autojuht <38987337+autojuht@users.noreply.github.com>
2019-03-02 21:32:56 +0200

    Update helmesChallenge.py

--- helmesChallenge.py
+++ helmesChallenge.py
@@ -1,11 +1,11 @@
 #!usr/bin/python3
 import time
 import argparse
-tstart = time.process_time()
 
 
 def anagramFinder(dictFilePath, word):
 
+    tstart = time.process_time()
     word = ''.join(word)
     dictFilePath = ''.join(dictFilePath)
     keyword = sorted(word.lower())
@@ -13,14 +13,14 @@ def anagramFinder(dictFilePath, word):
         words = f.read().splitlines()
 
     resList = ', '.join([w for w in words if sorted(str(w)) == keyword])
-    return resList
 
+    tstop = time.process_time()
+    return str(round((tstop - tstart) * 1000000, 2)) + ' ms, ' + resList
 
-tstop = time.process_time()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('dictFilePath', help='path to file', nargs=1, type=str)
     parser.add_argument('word', help='the word to find an anagram to', nargs=1, type=str)
     args = parser.parse_args()
-    print(str(round((tstop - tstart) * 1000000, 2)) + ' ms,', anagramFinder(args.dictFilePath, args.word))
+    print(anagramFinder(args.dictFilePath, args.word))
